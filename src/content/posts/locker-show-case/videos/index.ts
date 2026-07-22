5f40c60f3a375c3466dfccc7c4b6dd39318d80e6
Abba710 <abbasaifer3@gmail.com>
2026-07-22 18:55:37 +0500

    fix(locker-show-case): compress video size

--- src/content/posts/locker-show-case/videos/index.ts
+++ src/content/posts/locker-show-case/videos/index.ts
@@ -1,18 +1,18 @@
-import pin from "./pin_1.mp4";
-import incognitoPermission from "./allowIncognito_1.mp4";
-import shortcuts from "./shortcut_1.mp4";
-import saveBookmarks from "./savebookmark_1.mp4";
-import createFolders from "./folders_1.mp4";
-import incognitoMode from "./incognitoShowcase_1.mp4";
-import search from "./search_1.mp4";
-import fullscreen from "./fullscrn_1.mp4";
-import customize from "./costumize_1.mp4";
-import importExport from "./importexport_1.mp4";
-import importBrowser from "./importfrombrowser_1.mp4";
-import sessionManager from "./sessionmanager_1.mp4";
-import qrGenerator from "./qr_1.mp4";
-import savePdf from "./pdf_1.mp4";
-import aiSummary from "./aisummary_1.mp4";
+import pin from "./pin_1_1.mp4";
+import incognitoPermission from "./allowIncognito_1_1.mp4";
+import shortcuts from "./shortcut_1_1.mp4";
+import saveBookmarks from "./savebookmark_1_1.mp4";
+import createFolders from "./folders_1_1.mp4";
+import incognitoMode from "./incognitoShowcase_1_1.mp4";
+import search from "./search_1_1.mp4";
+import fullscreen from "./fullscrn_1_1.mp4";
+import customize from "./costumize_1_1.mp4";
+import importExport from "./importexport_1_1.mp4";
+import importBrowser from "./importfrombrowser_1_1.mp4";
+import sessionManager from "./sessionmanager_1_1.mp4";
+import qrGenerator from "./qr_1_1.mp4";
+import savePdf from "./pdf_1_1.mp4";
+import aiSummary from "./aisummary_1_1.mp4";
 
 export {
   pin,
